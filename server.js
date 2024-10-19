d6e433f0605c1ac5154f4a8d8c2b61c0807702a0
fjoewq3 <bartholomewinner69@gmail.com>
2024-10-19 11:31:28 -0700

    Update server.js

--- server.js
+++ server.js
@@ -25,3 +25,9 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+const path = require('path');
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
