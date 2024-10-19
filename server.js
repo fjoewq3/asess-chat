0ea4723136aeb2767a16c7e6204eead081aed934
fjoewq3 <bartholomewinner69@gmail.com>
2024-10-19 11:24:49 -0700

    Update server.js

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ io.on('connection', (socket) => {
   });
 });
 
+console.log("Server is starting...");
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
