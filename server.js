a5a43d9c5fc4fa0f3fad05b2d5b8e29e2ed3f05d
fjoewq3 <bartholomewinner69@gmail.com>
2024-10-19 08:40:25 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,34 @@
+// server.js
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
 
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Serve a simple message for testing
+app.get('/', (req, res) => {
+    res.send('Socket.IO Chat Server is running');
+});
+
+// Handle socket connections
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    // Listen for chat messages
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+
+    // Handle user disconnect
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
